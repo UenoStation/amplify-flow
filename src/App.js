dd201fa33976a41836c70ce125e3965e67b6241e
UenoStation <rappagariya@gmail.com>
2018-12-19 14:37:27 -0700

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 class App extends Component {
+  componentDidMount() {
+    if (Auth)
+      Auth
+        .currentSession()
+        .then(session => console.log(session))
+        .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
